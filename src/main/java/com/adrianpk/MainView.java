cdbb91a35b53570c9622b44ddca7b07fbcb2883b
Adrian PK <adrianpk@adrianpk.com>
2021-09-28 16:37:27 +0200

    Todo list bare implementation

--- src/main/java/com/adrianpk/MainView.java
+++ src/main/java/com/adrianpk/MainView.java
@@ -1,21 +1,40 @@
 package com.adrianpk;
 
+import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.server.PWA;
 import com.vaadin.flow.router.Route;
 
 /**
  * The main view contains a button and a click listener.
  */
-@Route
+@Route("")
 @PWA(name = "My Application", shortName = "My Application")
 public class MainView extends VerticalLayout {
 
     public MainView() {
-        Button button = new Button("Click me",
-                event -> Notification.show("Clicked!"));
-        add(button);
+        VerticalLayout todosList = new VerticalLayout();
+        TextField taskField = new TextField();
+        Button addButton = new Button("Add");
+
+        addButton.addClickShortcut(Key.ENTER);
+        addButton.addClickListener(click -> {
+            Checkbox checkbox = new Checkbox(taskField.getValue());
+            todosList.add(checkbox);
+        });
+        add(
+                new H1("Vaadin Todo"),
+                todosList,
+                new HorizontalLayout(
+                        taskField,
+                        addButton
+                )
+        );
     }
 }
