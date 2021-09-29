9335f4559ad2ca19f709fd87ca2a76235bb2da41
Adrian K <adrian@kugo.tech>
2021-09-29 23:09:53 +0200

    Playing with styles

--- src/main/java/com/adrianpk/MainView.java
+++ src/main/java/com/adrianpk/MainView.java
@@ -2,7 +2,9 @@ package com.adrianpk;
 
 import com.vaadin.flow.component.Key;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.checkbox.Checkbox;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -15,6 +17,7 @@ import com.vaadin.flow.router.Route;
  * The main view contains a button and a click listener.
  */
 @Route("")
+@CssImport("styles/shared-styles.css")
 @PWA(name = "My Application", shortName = "My Application")
 public class MainView extends VerticalLayout {
 
@@ -23,7 +26,10 @@ public class MainView extends VerticalLayout {
         TextField taskField = new TextField();
         Button addButton = new Button("Add");
 
+        addButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
         addButton.addClickShortcut(Key.ENTER);
+
         addButton.addClickListener(click -> {
             Checkbox checkbox = new Checkbox(taskField.getValue());
             todosList.add(checkbox);
